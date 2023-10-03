daa78d300da8319e57ccf2bfe3997264d25a71ed
PeterParkerOnly <ijusthopegithubworks@gmail.com>
2023-10-03 19:14:19 +1030

    Little Update

--- Assets/Scripts/EnemyScripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyScripts/EnemyMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    [SerializeField] private GameObject detectionZone;
     private float gapRange;
     public float speed;
     //[SerializeField] private float spottedRange = 15f;
@@ -14,22 +15,26 @@ public class EnemyMovement : MonoBehaviour
         gapRange = Vector2.Distance(transform.position, target.transform.position);
 
         Debug.Log(gapRange);
-        if (gapRange > 15)
+
+        if (gapRange > 5)
         {
             speed = 4;
         }
-        else if (gapRange <= 15)
+        else if (gapRange <= 5)
         {
             EnemyMove();
             speed = 5.5f;
         }
     }
 
-
-
     void EnemyMove()
     {
         transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+
+        //float AngleTarget = Mathf.Atan2(target.transform.position.y - transform.position.y, target.transform.position.x - transform.position.x);
+        //float AngleRotate = (180 / Mathf.PI) * AngleTarget;
+
+        //this.transform.rotation = Quaternion.Euler(0, 0, AngleRotate);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
